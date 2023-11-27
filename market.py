799acd8bb4f4a959ca227555c2f98d0be4231588
Senura Weerasinghe <senuranisal1998@gmail.com>
2023-11-27 23:17:31 +0000

    added template inheritance

--- market.py
+++ market.py
@@ -9,5 +9,10 @@ def home_page():
 
 
 @app.route('/market')
-def market():
-    return render_template('market.html')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html',items=items)
