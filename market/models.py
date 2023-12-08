1b8610cf509cdfa1866799bec0b395d07e2c9dbf
Senura Weerasinghe <senuranisal1998@gmail.com>
2023-12-08 21:49:52 +0000

    added user reletionship

--- market/models.py
+++ market/models.py
@@ -1,12 +1,13 @@
 # inherit from db.Model class
 from market import db
 
-class User(db.model):
+class User(db.Model):
    id = db.Column(db.Integer(), primary_key=True)
    username=db.Column(db.String(length=30),nullable=False,unique=True)
-   email_address=db_Column(db.String(length=50),nullable=False,unique=True)
-   password_hash=db.Column(db.String(length=60),nullable=false)
-   budget=db.Column(db.Integer(),nullable=false,default=1000)
+   email_address=db.Column(db.String(length=50),nullable=False,unique=True)
+   password_hash=db.Column(db.String(length=60),nullable=False)
+   budget=db.Column(db.Integer(),nullable=False,default=1000)
+   items=db.relationship('Item',backref='owned_user',lazy=True)
 
 
 class Item(db.Model):
@@ -16,6 +17,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner=db.Column(db.Integer(),db.ForeignKey('user.id'))
 
     #shows the string representation - mainly for debugging
     def __repr__(self):
